8aa9bf9b0813bfee7c3ef9cbb1fb7bef0bd62b86
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-09-03 17:02:29 -0300

    feat: create not found page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes as Switch, Route } from 'react-router-dom';
 import Favorites from './pages/Favorites';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" element={ <Home /> } />
       <Route path="favorites" element={ <Favorites /> } />
+      <Route path="*" element={ <NotFound /> } />
     </Switch>
   );
 };
