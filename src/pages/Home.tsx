a6e9f4f43fa2ce2565305fdd4e0577468c20b085
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-09-07 20:16:59 -0300

    feat: insert search bar to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import SearchBar from '../components/SearchBar';
 
 const Home: React.FC = () => {
   return (
     <div>
       <Header />
-      <p>Home Page</p>
+      <SearchBar />
     </div>
   );
 };
