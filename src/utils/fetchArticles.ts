1fc23c4a99a8b48239e7846c0eeb4e3b45c48495
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-09-08 17:01:34 -0300

    feat: update fetchArticles to handle pages

--- src/utils/fetchArticles.ts
+++ src/utils/fetchArticles.ts
@@ -2,9 +2,9 @@ import { IArticlesResponse } from '../interfaces/Article';
 
 const { REACT_APP_API_URL, REACT_APP_API_KEY } = process.env;
 
-const fetchArticles = async (query: string) => {
+const fetchArticles = async (query: string, page: string | number) => {
   const response = await fetch(
-    `${REACT_APP_API_URL}${query}?urls=true&apiKey=${REACT_APP_API_KEY}`,
+    `${REACT_APP_API_URL}${query}?urls=true&page=${page}&apiKey=${REACT_APP_API_KEY}&pageSize=10000`,
   );
 
   if (!response.ok) {
