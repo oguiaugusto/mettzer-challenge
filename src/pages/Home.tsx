85ce4ccacccc4eff7fa589cba04c8e894dd7f63b
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-09-08 17:03:23 -0300

    feat: insert pagination to articles search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import Article from '../components/Article';
 import Header from '../components/Header';
 import Loader from '../components/Loader';
 import SearchBar from '../components/SearchBar';
 import { IArticle } from '../interfaces/Article';
+import fetchArticles from '../utils/fetchArticles';
+import handlePages from '../utils/handlePages';
 import '../styles/Home.scss';
+import PaginationHandler from '../components/PaginationHandler';
 
 const Home: React.FC = () => {
   const [articles, setArticles] = useState<IArticle[]>([]);
   const [loading, setLoading] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = searchParams.get('page') || '1';
+
+  const handleSearch = async (query: string) => {
+    setLoading(true);
+    setArticles([]);
+
+    const articlesResponse = await fetchArticles(query, page);
+    setArticles(articlesResponse);
+
+    setLoading(false);
+  };
+  const handlePreviousPage = () => handlePages('PREVIOUS', searchParams, setSearchParams);
+  const handleNextPage = () => handlePages('NEXT', searchParams, setSearchParams);
+
+  useEffect(() => {
+    const query = searchParams.get('query') || '';
+
+    if (query && query.length > 0) {
+      handleSearch(query);
+    }
+  }, [page]);
 
   return (
     <div className="home-page">
       <Header />
-      <SearchBar setArticles={ setArticles } setLoading={ setLoading } />
-      <div className="articles">
+      <SearchBar handleSearch={ handleSearch } />
+      <div className="home-page__articles">
         { loading ? <Loader /> : null }
         {
           articles.map((article) => (
@@ -22,6 +48,17 @@ const Home: React.FC = () => {
           ))
         }
       </div>
+      {
+        articles.length > 0 ? (
+          <PaginationHandler
+            prefix="home-page"
+            page={ page }
+            handlePreviousPage={ handlePreviousPage }
+            handleNextPage={ handleNextPage }
+            articlesLength={ articles.length }
+          />
+        ) : null
+      }
     </div>
   );
 };
